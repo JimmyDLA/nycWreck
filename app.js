7ea0d9da4f3f55cfed4a93b259ffd11d986b4b8f
Jimmy De Los Angeles <jimmydelosangeles@gmail.com>
2017-03-13 16:38:02 -0400

    map and form added

--- app.js
+++ app.js
@@ -167,6 +167,26 @@ function scrollCont(){
   smoothScroll(3420)
 }
 
+function scrollMap(){
+  smoothScroll(3700)
+}
 function scrollTops(){
   smoothScroll(1);
+}
+
+
+// FUNCTION TO SLIDE THROUGH SLIDESHOW
+var slideIndex = 0;
+carousel();
+
+function carousel() {
+    var i;
+    var x = document.getElementsByClassName("mySlides");
+    for (i = 0; i < x.length; i++) {
+      x[i].style.display = "none";
+    }
+    slideIndex++;
+    if (slideIndex > x.length) {slideIndex = 1}
+    x[slideIndex-1].style.display = "block";
+    setTimeout(carousel, 4000); // Change image every 2 seconds
 }
