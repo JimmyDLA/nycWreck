a814c677bdcefd62432cfdc9b1dbc0ac04fb39a0
Jimmy De Los Angeles <jimmydelosangeles@gmail.com>
2017-03-20 16:37:13 -0400

    php added

---
+++ contact_me.js
@@ -0,0 +1,30 @@
+$(document).ready(function() {
+
+    $('form').on('submit', function(event) {
+        // Prevent default posting of form
+        event.preventDefault();
+
+        // Creating object with all of the form values
+        var data = {
+            name: document.getElementById('inputName').value,
+            email: document.getElementById('inputEmail').value,
+            subject: document.getElementById('inputSubject').value,
+            message: document.getElementById('inputMessage').value
+        };
+
+        // Making ajax call. Sending data from JS to PHP
+        $.ajax({
+            url: "./mail/contact_me.php",
+            type: "POST",
+            data: data
+        }).done(function(data) {
+            console.log('success!');
+            //clear all fields
+            $('#contactForm').trigger("reset");
+        }).fail(function() {
+            console.log('fail!');
+        });
+
+    });
+
+});
---
+++ mail/contact_me.php
@@ -0,0 +1,28 @@
+<?php
+
+// Check for empty fields. If empty stop the script
+if( empty($_POST['name'])    ||
+    empty($_POST['email'])   ||
+    empty($_POST['subject']) ||
+    empty($_POST['message']) ||
+    !filter_var($_POST['email'],FILTER_VALIDATE_EMAIL))
+    {
+        echo "No arguments Provided!";
+        return false;
+    }
+
+// Getting the values from the object that we sent
+$name = $_POST['name'];
+$email_address = $_POST['email'];
+$subject = $_POST['subject'];
+$message = $_POST['message'];
+
+// Create the email and send the message
+$to = 'jimmydelosangeles@me.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
+$email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nMessage: $message";
+$headers = "From: jimmydelosangeles@me.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
+$headers .= "Reply-To: $email_address";
+mail($to,$subject,$email_body,$headers);
+return true;
+
+?>
